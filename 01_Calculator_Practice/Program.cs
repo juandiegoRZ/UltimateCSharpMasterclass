b28501f724b25a0186e2221908fc0b54ec05a36b
Juan Diego Rodríguez Zumbado <juandirz@hotmail.com>
2025-03-08 20:20:31 -0600

    Practicas de cursos

---
+++ 01_Calculator_Practice/Program.cs
@@ -0,0 +1,49 @@
+﻿int firstNumber;
+int secondNumber;
+int result;
+string option;
+
+Console.WriteLine("Hello!");
+Console.WriteLine("Input the first number:");
+firstNumber = int.Parse(Console.ReadLine());
+Console.WriteLine("Input the second number:");
+secondNumber = int.Parse(Console.ReadLine());
+
+Console.WriteLine("What do you want to do with those numbers?");
+Console.WriteLine("[A]dd");
+Console.WriteLine("[S]ubtract");
+Console.WriteLine("[M]ultiply");
+option = Console.ReadLine();
+
+Calculation(firstNumber, secondNumber, option);
+
+void CloseApp()
+{
+	Console.WriteLine("Press any key to close.");
+	Console.ReadLine();
+}
+
+void Calculation(int a, int b, string selection)
+{
+	int result;
+	if (selection.ToUpper() == "A")
+	{
+		result = a + b;
+		Console.WriteLine($"{a} + {b} = {result}");
+	}
+	else if (selection.ToUpper() == "S")
+	{
+		result = a - b;
+		Console.WriteLine($"{a} - {b} = {result}");
+	}
+	else if (selection.ToUpper() == "M")
+	{
+		result = a * b;
+		Console.WriteLine($"{a} * {b} = {result}");
+	}
+	else
+	{
+		Console.WriteLine("Invalid option");
+	}
+	CloseApp();
+}
---
+++ WhileLoopPractice/Program.cs
@@ -0,0 +1,138 @@
+﻿using System;
+
+namespace Excercises
+{
+	public class Exercises
+	{
+		public static int CalculateSumOfNumbersBetween(int firstNumber, int lastNumber)
+		{
+			int sum = 0;
+			while (firstNumber <= lastNumber)
+			{
+				if (firstNumber == lastNumber)
+				{
+					sum = firstNumber;
+					return sum;
+				}
+				else
+				{
+					sum += firstNumber + lastNumber;
+					firstNumber++;
+					lastNumber--;
+				}
+			}
+			return sum;
+		}
+		public static string RepeatCharacter(char character, int targetLength)
+		{
+			string sum = "";
+			do
+			{
+				sum += character.ToString();
+			}
+			while (sum.Length < targetLength);
+			return sum;
+		}
+		public static int Factorial(int number)
+		{
+			int total = 1;
+			for (int i = 1; i <= number; i++)
+			{
+				total *= i;
+			}
+			return total;
+		}
+		public static string BuildHelloString()
+		{
+			char[] letters = {'h','e','l','l','o'};
+			var result = "";
+			for (int i = 0; i < letters.Length; i++)
+			{
+				result += letters[i];
+			}
+			return result;
+		}
+		public static bool IsWordPresentInCollection(string[] words, string wordToBeChecked)
+		{
+			for (int i = 0; i < words.Length; i++)
+			{
+				if (words[i] == wordToBeChecked)
+				{
+					return true;
+				}
+			}
+			return false;
+		}
+		public static int FindMax(int[,] numbers)
+		{
+			var arrayRow = numbers.GetLength(0);
+			var arrayColumn = numbers.GetLength(1);
+			var maxValue = 0;
+			var currentValue = 0;
+			if (arrayRow == 0 || arrayColumn == 0)
+			{
+				return -1;
+			}
+			else
+			{
+				maxValue = numbers[0, 0];
+				for (var i = 0; i < arrayRow; i++)
+				{
+					for (var j = 0; j < arrayColumn; j++)
+					{
+						currentValue = numbers[i, j];
+						if (currentValue > maxValue)
+						{
+							maxValue = currentValue;
+						}
+					}
+				}
+			}
+			return maxValue;
+		}
+		public static bool IsAnyWordLongerThan(int length, string[] words)
+		{
+			var wordsLength = words.GetLength(0);
+			bool result = true;
+			if (wordsLength == 0)
+			{
+				result = false;
+			}
+			else
+			{
+				foreach (var word in words)
+				{
+					if (Convert.ToInt32(word.Length) > length)
+						result = true;
+					else
+						result = false;
+				}
+			}
+			return result;
+		}
+		public List<string> GetOnlyUpperCaseWords(List<string> words)
+		{
+			var newWords = new List<string>();
+			foreach (var word in words)
+			{
+				if (newWords.Contains(word))
+				{
+					continue;
+				}
+				bool isUpperCase = true;
+				foreach (char c in word)
+				{
+					if (!char.IsUpper(c))
+					{
+						isUpperCase = false;
+					}
+				}
+				if (isUpperCase)
+				{
+					newWords.Add(word);
+				}
+			}
+			return newWords;
+		}
+	}
+}
